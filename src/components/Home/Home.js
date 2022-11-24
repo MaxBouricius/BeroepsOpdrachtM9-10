e0eaf4ec42130cc2fb0ec8b62fb8b7b6332d19e1
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2022-11-24 20:56:38 +0100

    Made progress with the PriceCard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,37 +2,41 @@ import "./Home.css"
 
 
 import FeatureCard from "../FeatureCard/FeatureCard";
+import PriceCard from "../PriceCard/PriceCard";
 
-const Home = (props) =>{
-    return(
+const Home = (props) => {
+    return (
         <>
-            <div className="home__header">
+            <section className="home__header">
 
-            </div>
-            <div className="home__over">
-                <div className="home__oversection1">
+            </section>
+            <section className="home__over">
+                <article className="home__oversection1">
                     <h2>Over Callizione</h2>
                     <p>Een tekst waarom jij Callizione zou moeten gebruiken </p>
-                </div>
-                <div className="home__oversection2">
-                    
-                </div>
-            </div>
+                </article>
+                <article className="home__oversection2">
+
+                </article>
+            </section>
             <section className="home__features">
                 <h2 className="home__name">Onze Features</h2>
                 <div className="home__featureCards">
-                <FeatureCard featureName="PC builder" featureInformation="Gebruik onze geoptimaliseerde PC builder om uw nieuwe computer samen te stellen!"/>
-                <FeatureCard featureName="24/7 tech support" featureInformation="Voor problemen met onze producten kunt u elk moment van de dag onze support center bellen!"/>
-                <FeatureCard featureName="Laptop kiezer" featureInformation="Met onze laptop kiezer kunt u de beste laptop vinden voor wat u nodig heeft!"/>
-                <FeatureCard featureName="Retour" featureInformation="Niet goed? geld terug, zolang het product nog functioneel is kunt u binnen 2 maanden een voledige refund krijgen!"/>
+                    <FeatureCard featureName="PC builder" featureInformation="Gebruik onze geoptimaliseerde PC builder om uw nieuwe computer samen te stellen!" />
+                    <FeatureCard featureName="24/7 tech support" featureInformation="Voor problemen met onze producten kunt u elk moment van de dag onze support center bellen!" />
+                    <FeatureCard featureName="Laptop kiezer" featureInformation="Met onze laptop kiezer kunt u de beste laptop vinden voor wat u nodig heeft!" />
+                    <FeatureCard featureName="Retour" featureInformation="Niet goed? geld terug, zolang het product nog functioneel is kunt u binnen 2 maanden een voledige refund krijgen!" />
                 </div>
             </section>
-            <div className="home__prijs">
+            <section className="home__prijs">
+                <h2 className="section__title">Onze support packages</h2>
+                <div className="home__featureCards">
+                    <PriceCard priceCardTitle="Support package BASIC"/>
+                </div>
+            </section>
+            <section className="home__contact">
 
-            </div>
-            <div className="home__contact">
-
-            </div>
+            </section>
         </>
     )
 }
--- src/components/PriceCard/PriceCard.js
+++ src/components/PriceCard/PriceCard.js
@@ -3,10 +3,9 @@ import "./PriceCard.css"
 const PriceCard = (props) => {
     return (
         <>
-            <article className="feature">
-                <img src="/img/featureFoto.png" alt="featureFoto" />
-                <h3 className="feature__name">{props.featureName}</h3>
-                <p className="feature__info">{props.featureInformation}</p>
+            <article className="priceCard">
+                <h3>{props.priceCardTitle}</h3>
+                
             </article>
         </>
     )
