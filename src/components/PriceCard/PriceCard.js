454ea446da1fb45903ee5a5f4a4c8b5aea4cb11e
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2022-11-25 09:44:52 +0100

    small edits

--- src/components/PriceCard/PriceCard.js
+++ src/components/PriceCard/PriceCard.js
@@ -2,12 +2,13 @@ import "./PriceCard.css"
 
 const PriceCard = (props) => {
     console.log(props.features)
+    console.t
     return (
         <>
             <article className="priceCard">
                 <h3>{props.priceCardTitle}</h3>
                 {props.features.map((feature) => (
-                    <div className="feature">{feature}</div>
+                    <div className="user">{feature}</div>
                 ))}
             </article>
         </>
